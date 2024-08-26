fdb9c5cdb156a4e24e6642a40bb8e5110e3b5fce
Severin Isermann <isermann.severin@gmail.com>
2024-08-26 18:33:02 +0200

    Added like Counter to like,unlike function. added book description to bookcards.

--- script.js
+++ script.js
@@ -191,27 +191,52 @@ function renderBooks() {
 function likeUnlike(indexBook){
     let id = document.getElementsByTagName('svg')[indexBook].id;
 
-    if (id == 'heart' + indexBook){
-        giveLike(indexBook);
+    if (id == 'heart' + [indexBook]){
+        giveLike(indexBook, id);
     } else if (id == 'heartfilled' + indexBook){
-        loseLike(indexBook);
+        loseLike(indexBook, id);
     }
 }
 
-function giveLike(indexBook){
+function giveLike(indexBook, id){
     let heartfill = document.getElementById('heart' + indexBook);
     heartfill.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
     ${indexBook}</svg>`;
     document.getElementById('heart' + indexBook).id = 'heartfilled' + indexBook;
+
+    raiseLikes(indexBook, id);
 }
 
-function loseLike(indexBook){
+function raiseLikes(indexBook, id){
+  let likeCounter = document.getElementById('likeCounter' + indexBook);
+  for (let i = 0; i < books[indexBook].likes; i++) {
+    const element = books[indexBook].likes;
+    if (id == 'heart' + indexBook) {
+    return likeCounter.innerHTML = element + 1;
+    }
+   
+  }
+}
+
+function loseLike(indexBook, id){
     let heartfill = document.getElementById('heartfilled' + indexBook);
     heartfill.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
   <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
 ${indexBook}</svg>`;
 document.getElementById('heartfilled' + indexBook).id = 'heart' + indexBook;
-}
-// Commentfunktion
 
+ReduceLikes(indexBook, id);
+}
+
+function ReduceLikes(indexBook, id){
+  let likeCounter = document.getElementById('likeCounter' + indexBook);
+  for (let i = 0; i < books[indexBook].likes; i++) {
+    const element = books[indexBook].likes;
+    if (id == 'heartfilled' + indexBook) {
+    return likeCounter.innerHTML = element;
+    }
+  }
+}
+
+// Commentfunktion
